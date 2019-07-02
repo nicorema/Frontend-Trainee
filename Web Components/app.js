6e1d2ec78126a00155dcbe33adee584689cf043d
nicorema <nicorema>
2019-07-02 17:16:43 -0500

    Web Components + Apuntes

---
+++ Web Components/app.js
@@ -0,0 +1,72 @@
+
+class PopUpInfo extends HTMLElement {
+    constructor() {
+        super();
+        var shadow = this.attachShadow({ mode: 'closed' });
+
+        // Create spans
+        var wrapper = document.createElement('span');
+        wrapper.setAttribute('class', 'wrapper');
+        var icon = document.createElement('span');
+        icon.setAttribute('class', 'icon');
+        icon.setAttribute('tabindex', 0);
+        var info = document.createElement('span');
+        info.setAttribute('class', 'info');
+
+        // Take attribute content and put it inside the info span
+        var text = this.getAttribute('text');
+        info.textContent = text;
+
+        // Insert icon
+        var imgUrl;
+        if (this.hasAttribute('img')) {
+            imgUrl = this.getAttribute('img');
+        } else {
+            imgUrl = 'img/default.png';
+        }
+        var img = document.createElement('img');
+        img.src = imgUrl;
+        icon.appendChild(img);
+        // Create some CSS to apply to the shadow dom
+        var style = document.createElement('style');
+
+        style.textContent = `
+            .wrapper {
+            position: relative;
+            }
+
+            .info {
+            font-size: 0.8rem;
+            width: 200px;
+            display: inline-block;
+            border: 1px solid black;
+            padding: 10px;
+            background: white;
+            border-radius: 10px;
+            opacity: 0;
+            transition: 0.6s all;
+            position: absolute;
+            bottom: 20px;
+            left: 10px;
+            z-index: 3;
+            }
+
+            img {
+            width: 1.2rem;
+            }
+
+            .icon:hover + .info, .icon:focus + .info {
+            opacity: 1;
+            }`;
+
+        shadow.appendChild(style);
+        shadow.appendChild(wrapper);
+        wrapper.appendChild(icon);
+        wrapper.appendChild(info);
+
+    }
+
+}
+
+//Registering the element
+customElements.define('nico-popup', PopUpInfo);
