ed6687afff9b48747c6fd6bd03c6efd1b0155b8c
Nicolás Restrepo <nicolas.restrepo.mantilla@gmail.com>
2019-10-28 16:57:35 -0500

    navigation items

--- udemy-course/burger-builder/src/components/Logo/Logo.js
+++ udemy-course/burger-builder/src/components/Logo/Logo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import burgerLogo from "../../assets/images/hamburger.png";
 import classes from "./Logo.module.css";
 const Logo = (props) => (
-    <div class={classes.Logo}>
+    <div className={classes.Logo}>
         <img src={burgerLogo} alt="Burger Logo"/>
     </div>
 );
--- udemy-course/burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ udemy-course/burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./Toolbar.module.css";
 import Logo from "../../Logo/Logo";
+import NavigationItems from "../NavigationItems/NavigationItems";
 const Toolbar = (props) => (
-    <header class={classes.Toolbar}>
+    <header className={classes.Toolbar}>
         <div>Menu</div>
         <Logo />
         <nav>
-            ...
+            <NavigationItems />
         </nav>
     </header>
 );
