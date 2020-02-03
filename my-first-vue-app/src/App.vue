ab9f2ff98e69ef11ae8021f1a40efa11e3e945e2
Nicolás Restrepo <nicolas.restrepo.mantilla@gmail.com>
2020-02-02 21:52:41 -0500

    learning vue

---
+++ my-first-vue-app/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
+  </div>
+</template>
+
+<script>
+import Todos from "./components/Todos";
+export default {
+  name: "app",
+  components: { Todos },
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "Todo One", completed: false },
+        { id: 2, title: "Todo Two", completed: true },
+        { id: 3, title: "Todo Three", completed: false }
+      ]
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(item => item.id != id);
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
+</style>
---
+++ my-first-vue-app/src/components/Todos.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="todos">
+    <div v-bind:key="todoItem.id" v-for="todoItem of todos">
+      <TodoItem v-bind:todoItem="todoItem" v-on:del-todo="$emit('del-todo',todoItem.id)" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from "./TodoItem";
+export default {
+  name: "Todos",
+  props: ["todos"],
+  components: {
+    TodoItem
+  }
+};
+</script>
+
+<style scoped>
+</style>
