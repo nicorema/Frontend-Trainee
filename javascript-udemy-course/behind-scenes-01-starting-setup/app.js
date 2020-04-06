5217ba7931e85be5731f53b00f2f4972daeeda9f
Nicolás Restrepo <nicolas.restrepo.mantilla@gmail.com>
2020-04-06 16:39:04 -0500

    es6

---
+++ javascript-udemy-course/behind-scenes-01-starting-setup/app.js
@@ -0,0 +1,19 @@
+// let name = 'Nico';
+// if(name === 'Nico'){
+//     let hobbies = ['sports','cooking'];
+//     console.log(hobbies);
+// }
+// function greet() {
+//   let age = 24;
+//   let name = 'Nico2';
+//   console.log(name, age);
+// }
+// console.log(name,hobbies);
+// greet();
+
+'use strict';
+
+let userName = 'Max';
+
+console.log(userName);
+
