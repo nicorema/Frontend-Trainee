9c476fdfab5275f1577948065183ffe760c47c03
Nicolas Restrepo <nicolas@correlation-one.com>
2022-04-18 19:07:35 -0500

    1

---
+++ react-query/base-blog-em/src/App.jsx
@@ -0,0 +1,18 @@
+import { Posts } from "./Posts";
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <h1>Blog Posts</h1>
+        <Posts />
+      </div>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
