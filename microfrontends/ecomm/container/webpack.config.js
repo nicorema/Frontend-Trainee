af076f17f5b7b1edb981b626a9d9c802054ed825
Nicolás Restrepo <nicolas@correlation-one>
2023-01-31 16:42:11 -0500

    Section 1: The Basics of Microfrontends

---
+++ microfrontends/ecomm/container/webpack.config.js
@@ -0,0 +1,10 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  devServer: {
+    port: 8080,
+  },
+
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
+};
