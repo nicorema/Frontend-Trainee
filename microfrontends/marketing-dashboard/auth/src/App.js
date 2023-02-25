feddcd5be81eb78afce5829b0ce4a25b20275115
Nicolás Restrepo <nicolas@correlation-one>
2023-02-25 10:59:43 -0500

    Section 12: Authentication in Microfrontends

--- microfrontends/marketing-dashboard/auth/src/App.js
+++ microfrontends/marketing-dashboard/auth/src/App.js
@@ -11,13 +11,17 @@ const generateClassName = createGenerateClassName({
   productionPrefix: "au",
 });
 
-export default ({ history }) => {
+export default ({ history, onSignIn }) => {
   return (
     <StylesProvider generateClassName={generateClassName}>
       <Router history={history}>
         <Switch>
-          <Route exact path="/auth/signin" component={Signin} />
-          <Route exact path="/auth/signup" component={Signup} />
+          <Route exact path="/auth/signin">
+            <Signin onSignIn={onSignIn} />
+          </Route>
+          <Route exact path="/auth/signup">
+            <Signup onSignIn={onSignIn} />
+          </Route>
         </Switch>
       </Router>
     </StylesProvider>
--- microfrontends/marketing-dashboard/auth/src/bootstrap.js
+++ microfrontends/marketing-dashboard/auth/src/bootstrap.js
@@ -3,18 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { createMemoryHistory, createBrowserHistory } from "history";
 
-const mount = (el, { onNavigate, defaultHistory, initialPath }) => {
+const mount = (el, { onNavigate, defaultHistory, initialPath, onSignIn }) => {
   const history =
     defaultHistory || createMemoryHistory({ initialEntries: [initialPath] });
   if (onNavigate) {
     history.listen(onNavigate);
   }
-  ReactDOM.render(<App history={history} />, el);
+  ReactDOM.render(<App onSignIn={onSignIn} history={history} />, el);
 
   return {
     onParentNavigate: ({ pathname: nextPathname }) => {
       const { pathname } = history.location;
-      console.log(nextPathname);
       if (pathname !== nextPathname) {
         history.push(nextPathname);
       }
