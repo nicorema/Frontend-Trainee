f612d58c2ce0646b4c7468ded4a387198fb451cd
Nicolás <nicolas@correlation-one.com>
2023-04-05 17:11:35 -0500

    tsc

---
+++ Mastering TypeScript - 2023 Edition/configuration/dist/farmstand.js
@@ -0,0 +1,2 @@
+"use strict";
+const printProduct = (product) => console.log(`${product.name} - $${product.price}`);
---
+++ Mastering TypeScript - 2023 Edition/configuration/dist/index.js
@@ -0,0 +1,6 @@
+"use strict";
+const norma = {
+    breed: "Silkie",
+    eggsPerWeek: 4,
+    name: "Norma",
+};
