2bb662ab38167d3b383f6f2908f0f019ba26c10d
Nicolás Restrepo <nicolas@correlation-one>
2023-05-22 15:31:01 -0500

    Section 1: Getting Started

---
+++ svelte/first-app/src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+  let name = "Nico";
+  let age = 27;
+
+  const incrementAge = () => (age += 1);
+</script>
+
+<h1>Hello {name}, my name is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
